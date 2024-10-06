fe3eec4a17d024842c76bbdf04770562649debb9
crono1105 <benja.gonzalezs@duocuc.cl>
2024-10-06 16:26:53 -0300

    control login

--- 1-proyecto/bookshare/api/controller.js
+++ 1-proyecto/bookshare/api/controller.js
@@ -5,36 +5,7 @@ const db = require('../api/db');
 
 const router = express.Router();
 
-// Ruta para registrar un nuevo usuario con la contraseña encriptada
-router.post('/usuarios', async (req, res) => {
-  const { nombre_usuario, correo, contrasena, ubicacion } = req.body; 
 
-  try {
-    // Verifica si el correo ya está registrado
-    const [existingUser] = await db.query('SELECT * FROM usuarios WHERE correo = ?', [correo]);
-
-    if (existingUser.length > 0) {
-      return res.status(400).json({ message: 'El correo ya está registrado.' });
-    }
-
-    // Encripta la contraseña antes de guardarla en la base de datos
-    const hashedPassword = await bcrypt.hash(contrasena, 10); // Usa 'contrasena' aquí
-
-    // Inserta el nuevo usuario en la base de datos
-    await db.query(
-      `INSERT INTO usuarios (nombre_usuario, correo, contrasena, rol, ubicacion, creado_en, reputacion) 
-       VALUES (?, ?, ?, 'normal', ?, NOW(), 0)`, // Cambia 'contraseña' por 'contrasena'
-      [nombre_usuario, correo, hashedPassword, ubicacion] // Usa 'hashedPassword' aquí
-    );
-
-    return res.status(201).json({ message: 'Usuario creado exitosamente.' });
-  } catch (error) {
-    console.error('Error al registrar usuario:', error);
-    return res.status(500).json({ message: 'Error interno del servidor.' });
-  }
-});
-
-// Ruta para iniciar sesión
 router.post('/login', async (req, res) => {
   const { correo, contrasena } = req.body; // Usa 'contrasena' aquí
 
@@ -60,7 +31,17 @@ router.post('/login', async (req, res) => {
     // Genera un token JWT
     const token = jwt.sign({ id: foundUser.id_usuario }, 'clave_unica', { expiresIn: '1h' });
 
-    return res.status(200).json({ message: 'Inicio de sesión exitoso', token });
+
+    return res.status(200).json({
+      message: 'Inicio de sesión exitoso',
+      token,
+      user: {
+        id_usuario: foundUser.id_usuario,
+        nombre_usuario: foundUser.nombre_usuario,
+        correo: foundUser.correo,
+        rol: foundUser.rol,
+      },
+    });
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
     return res.status(500).json({ message: 'Error interno del servidor.' });
