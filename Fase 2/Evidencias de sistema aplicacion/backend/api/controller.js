536b92f7c342bc47af72efbb36f696980ec17cf0
crono1105 <benja.gonzalezs@duocuc.cl>
2024-10-10 16:03:44 -0300

    usuario

--- Fase 2/Evidencias de sistema aplicacion/backend/api/controller.js
+++ Fase 2/Evidencias de sistema aplicacion/backend/api/controller.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const db = require('../api/db'); // Asumiendo que tienes un archivo de configuración de base de datos
+const db = require('../api/db'); 
 
 const router = express.Router();
 
-// Ruta para registrar un nuevo usuario con la contraseña encriptada
+
 router.post('/usuarios', async (req, res) => {
   const { nombre_usuario, correo, contrasena, ubicacion } = req.body;
 
   try {
-    // Verifica si el correo ya está registrado
+    
     const [existingUser] = await db.query('SELECT * FROM usuarios WHERE correo = ?', [correo]);
 
     if (existingUser.length > 0) {
       return res.status(400).json({ message: 'El correo ya está registrado.' });
     }
 
-    // Encripta la contraseña antes de guardarla en la base de datos
+    
     const hashedPassword = await bcrypt.hash(contrasena, 10);
 
-    // Inserta el nuevo usuario en la base de datos
+ 
     await db.query(
       `INSERT INTO usuarios (nombre_usuario, correo, contrasena, rol, ubicacion, creado_en, reputacion) 
        VALUES (?, ?, ?, 'normal', ?, NOW(), 0)`,
@@ -39,25 +39,25 @@ router.post('/login', async (req, res) => {
   const { correo, contrasena } = req.body;
 
   try {
-    // Busca el usuario en la base de datos
+    
     const [users] = await db.query('SELECT * FROM usuarios WHERE correo = ?', [correo]);
 
-    // Verifica si se encontró el usuario
+   
     if (!users || users.length === 0) {
       return res.status(401).json({ message: 'Credenciales incorrectas.' });
     }
 
-    // Extrae el primer usuario encontrado
+    
     const foundUser = users[0];
 
-    // Verifica la contraseña ingresada contra el hash almacenado
+    
     const isValid = await bcrypt.compare(contrasena, foundUser.contrasena);
 
     if (!isValid) {
       return res.status(401).json({ message: 'Credenciales incorrectas.' });
     }
 
-    // Genera un token JWT
+    
     const token = jwt.sign({ id: foundUser.id_usuario }, 'clave_unica', { expiresIn: '1h' });
 
     return res.status(200).json({
