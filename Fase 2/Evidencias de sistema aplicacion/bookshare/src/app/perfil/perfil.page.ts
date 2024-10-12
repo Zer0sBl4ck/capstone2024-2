f7450d12067ce6fdbde2cf3d17fe078187e80e62
crono1105 <benja.gonzalezs@duocuc.cl>
2024-10-12 19:04:40 -0300

    perfil

--- Fase 2/Evidencias de sistema aplicacion/bookshare/src/app/perfil/perfil.page.ts
+++ Fase 2/Evidencias de sistema aplicacion/bookshare/src/app/perfil/perfil.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router'; // Asegúrate de importar Router
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -15,13 +15,14 @@ export class PerfilPage implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router // Inyecta Router
   ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.correo = params.get('correo') || '';
-      this.correoLogueado = this.authService.getUserData()?.correo || null;  // Obtener el correo del usuario logueado
+      this.correoLogueado = this.authService.getUserEmail(); // Obtener el correo del usuario logueado
       if (this.correo) {
         this.authService.getUserProfile(this.correo).subscribe(
           (data) => {
@@ -37,6 +38,13 @@ export class PerfilPage implements OnInit {
 
   // Método para comparar si el usuario actual está viendo su propio perfil
   esMiPerfil(): boolean {
-    return this.correo === this.correoLogueado;
+    return this.correo === this.correoLogueado; // Verificar si el correo actual es igual al del usuario logueado
+  }
+
+  // Método para manejar el clic en el botón de editar perfil
+  editarPerfil(): void {
+    if (this.esMiPerfil()) { // Verifica que sea su propio perfil
+      this.router.navigate(['/update-perfil', this.correo]); // Navega a la ruta de edición
+    }
   }
 }
