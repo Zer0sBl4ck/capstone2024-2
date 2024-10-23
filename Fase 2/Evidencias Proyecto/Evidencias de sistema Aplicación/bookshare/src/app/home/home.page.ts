609b62c96df36ac7cd9cf2acbe9ebe80fa02410b
LuisAriasCl <lu.arias@duocuc.cl>
2024-10-23 15:00:31 -0300

    Homepage fixed

--- Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicación/bookshare/src/app/home/home.page.ts
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicación/bookshare/src/app/home/home.page.ts
@@ -2,22 +2,12 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 
-interface Libro {
-  isbn: string;
-  titulo: string;
-  autor: string;
-  descripcion: string;
-  genero: string;
-  imagen_libro_base64?: string; // Si la imagen es opcional
-}
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  libros: Libro[] = []; // Aquí almacenaremos los libros
   isLoggedIn = false;  
   userRole: string | null = null;  
   userEmail: string | null = null;  
@@ -26,7 +16,6 @@ export class HomePage {
 
   ionViewWillEnter() {
     this.checkAuthentication();
-    this.cargarLibros(); // Cargar libros al ingresar a la vista
   }
 
   checkAuthentication() {
@@ -39,22 +28,6 @@ export class HomePage {
     } 
   }
 
-  cargarLibros() {
-    this.authService.getLibros().subscribe(
-      (data) => {
-        this.libros = data.map((libro: any) => {
-          if (libro.imagen_libro) {
-            libro.imagen_libro_base64 = `data:image/jpeg;base64,${libro.imagen_libro}`;
-          }
-          return libro;
-        });
-      },
-      (error) => {
-        console.error('Error al cargar libros:', error);
-      }
-    );
-  }
-
   goToProfile() {
     if (this.userEmail) {
       this.router.navigate(['/perfil', this.userEmail],{ replaceUrl: true }); 
@@ -62,23 +35,26 @@ export class HomePage {
   }
 
   goAddBook(){
-    this.router.navigate(['/addbook'],{replaceUrl:true});
+    this.router.navigate(['/addbook'],{replaceUrl:true})
   }
-
   goAddBookuser(){
-    this.router.navigate(['/addbook-user'],{replaceUrl:true});
+    this.router.navigate(['/addbook-user'],{replaceUrl:true})
   }
-
   solicitudesadd(){
-    this.router.navigate(['/listar-libro-solicitud'],{replaceUrl:true});
+    this.router.navigate(['/listar-libro-solicitud'],{replaceUrl:true})
   }
 
   gosoliuser(){
-    this.router.navigate(['/solicitud-user'],{replaceUrl:true});
+    this.router.navigate(['/solicitud-user'],{replaceUrl:true})
   }
+  
 
   goBookOwner(){
-    this.router.navigate(['/bookowner', this.userEmail],{replaceUrl: true});
+    this.router.navigate(['/bookowner',this.userEmail],{replaceUrl: true})
+  }
+
+  gochat(){
+    this.router.navigate(['/chat'],{replaceUrl: true})
   }
 
   logout() {
