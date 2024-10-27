741e7d6e3bcff0c44d254f33bb1e8ed388a14c92
LuisAriasCl <lu.arias@duocuc.cl>
2024-10-27 17:44:58 -0300

    Swiper Libros Base de datos

--- Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicación/bookshare/src/app/home/home.page.ts
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicación/bookshare/src/app/home/home.page.ts
@@ -4,8 +4,12 @@ import { Router } from '@angular/router';
 import { IonicSlides } from '@ionic/angular';
 
 interface Book {
-  title: string;
-  image: string;
+  isbn: string;
+  titulo: string;
+  autor: string;
+  descripcion: string;
+  genero: string;
+  imagen_libro: string;
   selectedRating: number; // Almacena la calificación seleccionada para cada libro
 }
 
@@ -19,37 +23,15 @@ export class HomePage {
   userRole: string | null = null;  
   userEmail: string | null = null;  
   userName: string | null = null;
-  stars: number[] = Array(5).fill(0); // Un array para representar las 5 estrellas
+  stars: number[] = Array(5).fill(0);
   swiperModules = [IonicSlides];
+  books: Book[] = []; 
 
-  // Lista de libros con calificaciones
-  books: Book[] = [
-    {
-      title: 'Harry Potter y las reliquias 2',
-      image: 'assets/imagenes/harrypotterreliquias.png',
-      selectedRating: 0,
-    },
-    {
-      title: 'Como agua para chocolate',
-      image: 'assets/imagenes/comoaguaparachocolate.png',
-      selectedRating: 0,
-    },
-    {
-      title: 'Harry Potter y las reliquias',
-      image: 'assets/imagenes/harrypotterreliquias.png',
-      selectedRating: 0,
-    },
-    {
-      title: 'Harry Potter y las reliquias Parte 3',
-      image: 'assets/imagenes/harrypotterreliquias.png',
-      selectedRating: 0,
-    },
-  ];
-  
   constructor(private authService: AuthService, private router: Router) {}
 
   ionViewWillEnter() {
     this.checkAuthentication();
+    this.getLibros();
   }
 
   checkAuthentication() {
@@ -62,10 +44,31 @@ export class HomePage {
       this.userName = this.authService.getUserName();
     } 
   }
+  ngOnInit() {
+    this.getLibros(); // Cargar libros al inicializar la página
+  }
 
+  getLibros() {
+    this.authService.getLibros().subscribe(
+      (response) => {
+        console.log('Libros recibidos:', response); // Verifica la respuesta
+        this.books = response.map((libro: any) => ({
+          isbn: libro.isbn,
+          titulo: libro.titulo,
+          imagen_libro: libro.imagen_libro_base64 ? 'data:image/jpeg;base64,' + libro.imagen_libro_base64 : 'assets/imagenes/default.png', // Usa imagen_libro_base64 aquí
+          selectedRating: 0, // Calificación inicial
+        }));
+      },
+      (error) => {
+        console.error('Error al cargar los libros:', error);
+      }
+    );
+  }
+
+  
   rateBook(bookIndex: number, rating: number) {
     this.books[bookIndex].selectedRating = rating; // Actualiza la calificación seleccionada del libro
-    console.log(`Calificación seleccionada para ${this.books[bookIndex].title}: ${rating}`);
+    console.log(`Calificación seleccionada para ${this.books[bookIndex].titulo}: ${rating}`);
     // Aquí puedes agregar lógica adicional, como guardar la calificación en una base de datos
   }
 
