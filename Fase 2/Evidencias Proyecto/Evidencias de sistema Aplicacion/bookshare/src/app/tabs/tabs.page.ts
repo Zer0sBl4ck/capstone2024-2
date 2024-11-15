cebcc4c1285153592bdf0c01d39cca3bf0fe0374
tallarin <benja.gonzalezs@duocuc.cl>
2024-11-15 19:10:20 -0300

    tab

--- Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/bookshare/src/app/tabs/tabs.page.ts
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/bookshare/src/app/tabs/tabs.page.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage implements OnInit, OnDestroy {
+  isLoggedIn = false;  // Esta variable controla si el usuario está logueado o no
+  userRole: string | null = null;
   notificacionesNoLeidas: number = 0;
   private intervalId: any;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
+    this.checkAuthentication();
     this.cargarNotificacionesNoLeidas();
-    // Realizar polling cada 30 segundos
+    // Realizar polling cada 5 segundos
     this.intervalId = setInterval(() => {
       this.cargarNotificacionesNoLeidas();
-    }, 5000); // 30000ms = 30 segundos
+    }, 5000); // 5000ms = 5 segundos
   }
 
   ngOnDestroy() {
@@ -25,20 +29,32 @@ export class TabsPage implements OnInit, OnDestroy {
       clearInterval(this.intervalId);  // Limpiar el intervalo cuando el componente se destruye
     }
   }
+
+  checkAuthentication() {
+    const token = this.authService.getToken();
+    this.isLoggedIn = !!token;  // Si el token existe, el usuario está logueado
+    if (this.isLoggedIn) {
+      this.userRole = this.authService.getUserRole();  // Obtener el rol del usuario (admin o usuario)
+    }
+  }
+
   cargarNotificacionesNoLeidas(): void {
-    const correo = this.authService.getUserEmail();  // Obtener el correo del usuario actual
+    const correo = this.authService.getUserEmail();
     if (correo) {
       this.authService.obtenerNotificaciones(correo).subscribe(
         (notificaciones: any[]) => {
-          // Contar las notificaciones no leídas
           this.notificacionesNoLeidas = notificaciones.filter(notificacion => !notificacion.visto).length;
         },
         (error) => {
           console.error('Error al obtener notificaciones:', error);
         }
       );
-    } else {
-      console.warn('No se pudo obtener el correo del usuario');
     }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.checkAuthentication();
+    window.location.reload();
   }
 }
